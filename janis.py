2ed70bae2a0c8daeed0df0e9585a8abf2aa55f79
Jonus228 <113671412+Jonus228@users.noreply.github.com>
2023-02-10 13:25:23 +0200

    JANIS MALECKIS

---
+++ janis.py
@@ -0,0 +1,9 @@
+v = input("Ievadi vārdu un uzvārdu: ")
+varda_burts = v[0].upper() + "."
+uzvards = v.split()[1].upper() + "."
+v_gatavs = varda_burts+uzvards
+print(v_gatavs)
+print(uzvards)
+
+
+ 
